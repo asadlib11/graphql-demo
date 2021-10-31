655110fd9e37885bffab3a94a9f5f3816aaaf277
Asad Ali Baloch <asadalibaloch059@gmail.com>
2021-10-31 22:37:37 +0500

    applied base functionality of using graphiql

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
+const app = express();
+
+//graphql
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: ()=> 'Hello World'
+            }
+        })
+    })
+});
+
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
